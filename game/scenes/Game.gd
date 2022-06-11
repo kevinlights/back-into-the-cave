6d4b72001daeb2372b4332ab76378cba8492c0c1
John Gabriel <accounts@johngabriel.co.uk>
2022-06-11 22:54:51 +0100

    Add intro sequence

--- game/scenes/Game.gd
+++ game/scenes/Game.gd
@@ -3,6 +3,10 @@ extends Spatial
 onready var camera_arm : Spatial = $CameraArm
 
 func _physics_process(delta : float) -> void:
+	return
 	camera_arm.rotate_z(delta * 0.06125)
 	camera_arm.rotate_x(delta * 0.15)
 	camera_arm.rotate_y(delta * 0.25)
+
+func _ready() -> void:
+	$AnimationPlayer.play("default")
