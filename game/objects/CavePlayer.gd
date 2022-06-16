6f6f9b4f64deef22bff6ed7fc60921198f918748
John Gabriel <accounts@johngabriel.co.uk>
2022-06-16 20:25:03 +0100

    Enable cave player to walk up slopes

--- game/objects/CavePlayer.gd
+++ game/objects/CavePlayer.gd
@@ -5,6 +5,7 @@ class_name CavePlayer
 const _Dust : PackedScene = preload("res://objects/Dust.tscn")
 
 const MOVE_SPEED : float = 40.0
+const HILL_CLIMB_SPEED : float = 20.0
 const JUMP_SPEED : float = 128.0
 const FALL_INCR : float = 386.0
 const MAX_FALL_SPEED : float = 160.0
@@ -22,14 +23,21 @@ func do_the_dusty() -> void:
 		dust.global_position = self.global_position + Vector2(0, 4) + (dir * 4.0)
 		dust.velocity = dir * 16.0
 
+func move(direction : Vector2, delta : float) -> void:
+	var collision : KinematicCollision2D = move_and_collide(MOVE_SPEED * direction * delta)
+	if collision != null and collision.normal.snapped(Vector2(0.25, 0.25)).y == -0.75:
+		position.y -= HILL_CLIMB_SPEED * delta
+		move_and_collide(HILL_CLIMB_SPEED * direction * delta)
+		velocity.y = 0.0
+
 func _physics_process(delta : float) -> void:
 	if Input.is_action_just_pressed("ui_up") and test_move(transform, Vector2.DOWN):
 		velocity = Vector2(0, -JUMP_SPEED)
 	if Input.is_action_pressed("ui_left"):
-		move_and_collide(MOVE_SPEED * Vector2.LEFT * delta)
+		move(Vector2.LEFT, delta)
 		sprite.flip_h = true
 	if Input.is_action_pressed("ui_right"):
-		move_and_collide(MOVE_SPEED * Vector2.RIGHT * delta)
+		move(Vector2.RIGHT, delta)
 		sprite.flip_h = false
 	
 	velocity.y += FALL_INCR * delta
@@ -43,6 +51,8 @@ func _physics_process(delta : float) -> void:
 			velocity = Vector2.ZERO
 		elif collision.normal == Vector2.DOWN:
 			velocity = Vector2.ZERO
+		elif collision.normal.snapped(Vector2(0.25, 0.25)).y == -0.75:
+			velocity.y = 0.0
 	
 	sprite_mirror_l.flip_h = sprite.flip_h
 	sprite_mirror_r.flip_h = sprite.flip_h
