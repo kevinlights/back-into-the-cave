02f8d9e17f842b7fd8649bf80d524777c14ae906
John Gabriel <accounts@johngabriel.co.uk>
2022-06-18 14:28:04 +0100

    Implement coins

---
+++ game/objects/Coin.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+const _Twinkle : PackedScene = preload("res://objects/Twinkle.tscn")
+
+onready var sprite : Sprite = $Sprite
+onready var timer_next_frame : Timer = $Timer_NextFrame
+
+func _on_Coin_body_entered(body) -> void:
+	if body is CavePlayer:
+		for dir in range(0.0, 360.0, 45.0):
+			var twinkle : Sprite = _Twinkle.instance()
+			get_parent().add_child(twinkle)
+			twinkle.global_position = self.global_position + Vector2(4, 4)
+			twinkle.velocity = Vector2.RIGHT.rotated(deg2rad(dir)) * 32.0
+		queue_free()
+
+func _on_Timer_NextFrame_timeout() -> void:
+	sprite.frame = wrapi(sprite.frame + 1, 0, 10)
---
+++ game/objects/Twinkle.gd
@@ -0,0 +1,12 @@
+extends Sprite
+
+var velocity : Vector2
+
+func _on_Timer_NextFrame_timeout() -> void:
+	if frame == 5:
+		queue_free()
+	else:
+		frame += 1
+
+func _physics_process(delta : float) -> void:
+	position += velocity * delta
